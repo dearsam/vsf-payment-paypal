6624d30312e4d768af910fd0637cca38152482c2
Max Malm <benjick@dumfan.net>
2019-09-03 12:23:54 +0200

    Add currency code to sdk url

--- hooks/beforeRegistration.ts
+++ hooks/beforeRegistration.ts
@@ -1,3 +1,4 @@
+import { currentStoreView } from '@vue-storefront/core/lib/multistore'
 
 export function beforeRegistration({ Vue, config, store, isServer }) {
   const VSF_PAYPAL_CODE = 'vsfpaypal'
@@ -13,8 +14,10 @@ export function beforeRegistration({ Vue, config, store, isServer }) {
   }
 
   if (!Vue.prototype.$isServer) {
+    const storeView = currentStoreView()
+    const { currencyCode } = storeView.i18n
     const clientId = config.paypal.clientId
-    const sdkUrl = `https://www.paypal.com/sdk/js?client-id=${clientId}`
+    const sdkUrl = `https://www.paypal.com/sdk/js?client-id=${clientId}&currency=${currencyCode}`
     var script = document.createElement('script')
     script.setAttribute('src', sdkUrl)
     document.head.appendChild(script)
